bd7ea642a9ac25ca47048966a2055559175fd7b5
Alif Ayodya <alif.ayodya@gmail.com>
2022-08-22 09:56:30 +0700

    adding files

--- script.js
+++ script.js
@@ -1,87 +1,49 @@
 const btn = document.querySelectorAll('.item');
 const scr = document.querySelector('.screen')
-var result = 0;
-let resultS = '';
-var counter = 0;
+
+let operator ='';
+let num = [];
+let execute = false;
+
 btn.forEach(btn => btn.addEventListener('click', ()=>{
-    
-   let hello = btn.textContent;
-    
-    if (hello != "="){
+    if(btn.textContent != "+" && btn.textContent != '-' && btn.textContent != '='
+    && btn.textContent != '/' && btn.textContent != '*'){
+        let number = parseInt(btn.textContent);
+        num.push(number);
+        console.log(num);
+    }
+    if (btn.textContent === "+"){
+        operator = "+";
         
-    resultS+=hello;
-    scr.textContent = resultS;
-    }
-    
-    if(hello === "=" ){
-
-        calculate(resultS);
-
-    }
-    if(hello === "-" ){
-
-        sub(resultS);
-
-    }
-    if(hello === '+'){
-        
-       add(resultS);
-    }
-    if(hello ==="clear"){
-        resultS='';
-        result=0;
-        scr.textContent = result;
-    }
-    
-}
-));
-
-var executed = false;
-function add(str){
-    if (executed === true){
-        
-          var b = str.split('+').map(Number);
-          for(i in b){
-            result+=b[i];
-            
-          }
-          console.log(result);
-          result = 0;
-          console.log(b);
-        executed = false;
-    }
-    
-    executed = true;
-  }
-function sub(str){
-
-    if (executed === true){
-        
-        var b = str.split('-').map(Number);
-            let help = b.reduce((total,num)=>{
-                return total -num;
-            });
-            
-       console.log(help);
-       result = 0;
-       console.log(b);
-      executed = false;
+        if(execute === true){
+            calculate();
         }
-        executed = true;
+        execute = true;
     }
-  
-
-  
-
-  
-
-function calculate(str){
-    var b = str.split('+').map(Number);
-    for(i in b){
-      result+=b[i];
-      
+    if (btn.textContent === "-"){
+        operator = "-";
+        if(execute === true){
+            calculate();
+        }
+        execute = true;
     }
-    
-    scr.textContent = result;
-    result = 0;
+
+}));
+
+function calculate(){
+
+    switch(operator){
+        case "+":
+               let res = num.reduce((total, num) =>{
+                return total + num;
+              });
+              
+              scr.textContent = res;
+              console.log(res);
+              break;
+              
+
+    }
+    execute = false;
+
 }
