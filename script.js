f09b75fdca379266e992ba1980d8cb25d361047f
Alif Ayodya <alif.ayodya@gmail.com>
2022-08-22 09:03:59 +0700

    adding files

--- script.js
+++ script.js
@@ -2,35 +2,86 @@ const btn = document.querySelectorAll('.item');
 const scr = document.querySelector('.screen')
 var result = 0;
 let resultS = '';
+var counter = 0;
 btn.forEach(btn => btn.addEventListener('click', ()=>{
     
-    hello = btn.textContent;
-    scr.textContent = hello;
-    resultS+=hello;
-    if(hello === '+'){
+   let hello = btn.textContent;
+    
+    if (hello != "="){
         
-       add(resultS);
-    }
-
-    if (hello != "="  ){
+    resultS+=hello;
+    scr.textContent = resultS;
     }
     
     if(hello === "=" ){
 
-        console.log(resultS);
+        calculate(resultS);
 
     }
+    if(hello === "-" ){
+
+        sub(resultS);
+
+    }
+    if(hello === '+'){
+        
+       add(resultS);
+    }
+    if(hello ==="clear"){
+        resultS='';
+        result=0;
+        scr.textContent = result;
+    }
+    
 }
 ));
 
-function add(num){
-    if(num === "+"){
-        return;
+var executed = false;
+function add(str){
+    if (executed === true){
+        
+          var b = str.split('+').map(Number);
+          for(i in b){
+            result+=b[i];
+            
+          }
+          console.log(result);
+          result = 0;
+          console.log(b);
+        executed = false;
     }
-    let r = num;
-    r = r.replace('+','');
-    r = parseInt(r);
-    result+=r;
-    resultS='';
-    console.log(result);                                                    
+    
+    executed = true;
+  }
+function sub(str){
+
+    if (executed === true){
+        
+        var b = str.split('-').map(Number);
+            let help = b.reduce((total,num)=>{
+                return total -num;
+            });
+            
+       console.log(help);
+       result = 0;
+       console.log(b);
+      executed = false;
+        }
+        executed = true;
+    }
+  
+
+  
+
+  
+
+function calculate(str){
+    var b = str.split('+').map(Number);
+    for(i in b){
+      result+=b[i];
+      
+    }
+    
+    scr.textContent = result;
+    result = 0;
 }
